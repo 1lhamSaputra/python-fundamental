c194ffc21c5913ea0872cb8f50684f351c09e4aa
il_ham <ilham05saputra@gmail.com>
2023-03-26 09:53:05 +0700

    perulangan

---
+++ perulangan_for.py
@@ -0,0 +1,9 @@
+# Perulangan
+jumlah_buku = 10
+print('ibu berkata, "Baca semua bukumu"')
+
+jumlah_buku_dibaca = 0
+print(f"Jumlah buku dibaca {jumlah_buku}")
+
+for jumlah_buku in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku} sudah dibaca")
---
+++ perulangan_while.py
@@ -0,0 +1,12 @@
+# Perulangan
+jumlah_buku = 1000
+print('ibu berkata, "Baca semua bukumu"')
+
+jumlah_buku_dibaca = 0
+print(f"Jumlah buku dibaca {jumlah_buku}")
+
+while jumlah_buku_dibaca < jumlah_buku:
+    jumlah_buku_dibaca = jumlah_buku_dibaca + 1
+    print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
+
+print(f"Jumlah buku dibaca {jumlah_buku}")
