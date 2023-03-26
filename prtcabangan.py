5420c57be3409cd5e89818547b2b5ea1dee44344
il_ham <ilham05saputra@gmail.com>
2023-03-26 09:27:19 +0700

    percabangan

---
+++ prtcabangan.py
@@ -0,0 +1,11 @@
+# percabangan
+jumlah_botol_sisi = 12
+
+print(f"Jumlah botol susu {jumlah_botol_sisi}")
+
+if jumlah_botol_sisi > 0:
+    print("Budi membeli 1 botol susu")
+else :
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang kerumah")
